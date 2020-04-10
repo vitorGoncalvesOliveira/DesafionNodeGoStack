3de010ec935dd95ab59e3c74718f91aa600f834a
vitor goncalves <vitorgoncalves.991@gmail.com>
2020-04-09 21:32:49 -0300

    Desafio 01 Node da nova jornada BootCamp

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,60 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const newRepository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+  repositories.push(newRepository)
+
+  return response.json(newRepository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const idx = repositories.findIndex((repository) => repository.id === id);
+  if (idx < 0)
+    return response.status(400).json("Repository not found!");
+
+  repositories[idx] = ({ ...repositories[idx], title, url, techs })
+
+  return response.json(repositories[idx]);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const idx = repositories.findIndex((repository) => repository.id === id);
+  if (idx < 0)
+    return response.status(400).json("Repository not found!");
+
+  repositories.splice(idx, 1);
+  return response.status(204).send("");
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const idx = repositories.findIndex((repository) => repository.id === id);
+  if (idx < 0)
+    return response.status(400).json("Repository not found!");
+
+  repositories[idx].likes += 1;
+
+  return response.json(repositories[idx]);
+
 });
 
 module.exports = app;
